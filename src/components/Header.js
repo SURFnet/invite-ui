4e77c668b5c05842054b2a49693715dbf98158ed
Okke Harsta <oharsta@zilverline.com>
2021-12-17 20:11:36 +0100

    Navigate to home

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,12 +2,19 @@ import "./Header.scss";
 import {ReactComponent as Logo} from "../img/logo_eduID.svg";
 import Button from "./Button"
 import I18n from "i18n-js";
+import {useNavigate} from "react-router-dom";
+import {stopEvent} from "../utils/forms";
 
 const Header = ({user}) => {
 
+    const navigate = useNavigate();
+
     return (
         <div className="header">
-            <a href="/">
+            <a href="/" onClick={e => {
+                stopEvent(e);
+                navigate("/");
+            }}>
                 <Logo/>
             </a>
             <h1>{I18n.t("header.title")}</h1>
