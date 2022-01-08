c41265ed2814b9af67a70291cbc66f65465b3fb8
Okke Harsta <oharsta@zilverline.com>
2022-01-08 12:27:53 +0100

    Inlined variable

--- src/utils/aup.js
+++ src/utils/aup.js
@@ -1,10 +1,7 @@
 import {isEmpty} from "./forms";
 
-export const institutionMembershipsWithNoAup = user => {
-    const membershipsWithoutAup = user.institutionMemberships.filter(membership => {
+export const institutionMembershipsWithNoAup = user =>
+    user.institutionMemberships.filter(membership => {
         const institution = membership.institution;
         return !isEmpty(institution.aupUrl) && !user.aups.some(aup => aup.institutionId === institution.id && aup.version === institution.aupVersion);
     })
-    return membershipsWithoutAup;
-
-}
